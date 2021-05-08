91aaa5d2305eb61ddf5ddb5fe3d3ab1e3787be02
meri-rtx <82427039+meri-rtx@users.noreply.github.com>
2021-05-08 12:09:47 +0900

    Add files via upload
    jwkim

--- src/mk_news_model.py
+++ src/mk_news_model.py
@@ -1,1 +1,151 @@
-1
+import pandas as pd
+import numpy as np
+import urllib.request
+from konlpy.tag import Okt
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, Dense, LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+DEBUG = True
+okt = Okt()
+stopwords = ['의', '가', '이', '은', '들', '는', '좀', '잘', '걍', '과', '도', '를', '으로', '자', '에', '와', '한', '하다']
+
+
+def load_data():
+    urllib.request.urlretrieve("https://raw.githubusercontent.com/e9t/nsmc/master/ratings_train.txt",
+                               filename="ratings_train.txt")
+    urllib.request.urlretrieve("https://raw.githubusercontent.com/e9t/nsmc/master/ratings_test.txt",
+                               filename="ratings_test.txt")
+
+    return pd.read_table('ratings_train.txt'), pd.read_table('ratings_test.txt')
+
+
+def cleaning_data():
+    global train_data, test_data
+    train_data.drop_duplicates(subset=['document'], inplace=True)  # document 열에서 중복인 내용이 있다면 중복 제거
+    train_data = train_data.dropna(how='any')  # Null 값이 존재하는 행 제거
+    train_data['document'] = train_data['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]", "")
+    train_data['document'] = train_data['document'].str.replace('^ +', "")  # white space 데이터를 empty value로 변경
+    train_data['document'].replace('', np.nan, inplace=True)
+    train_data = train_data.dropna(how='any')
+
+    test_data.drop_duplicates(subset=['document'], inplace=True)  # document 열에서 중복인 내용이 있다면 중복 제거
+    test_data = test_data.dropna(how='any')  # Null 값이 존재하는 행 제거
+    test_data['document'] = test_data['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]", "")  # 정규 표현식 수행
+    test_data['document'] = test_data['document'].str.replace('^ +', "")  # 공백은 empty 값으로 변경
+    test_data['document'].replace('', np.nan, inplace=True)  # 공백은 Null 값으로 변경
+    test_data = test_data.dropna(how='any')  # Null 값 제거
+
+
+def tokenize_data():
+    X_train = []
+    for sentence in train_data['document']:
+        temp_X = okt.morphs(sentence, stem=True)  # 토큰화
+        temp_X = [word for word in temp_X if not word in stopwords]  # 불용어 제거
+        X_train.append(temp_X)
+
+    X_test = []
+    for sentence in test_data['document']:
+        temp_X = okt.morphs(sentence, stem=True)  # 토큰화
+        temp_X = [word for word in temp_X if not word in stopwords]  # 불용어 제거
+        X_test.append(temp_X)
+
+    return X_train, X_test
+
+
+def encoding_data():
+    global X_train, X_test
+    global vocab_size
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(X_train)
+    # 1차 
+    threshold = 3
+    total_cnt = len(tokenizer.word_index)  # 단어의 수
+    rare_cnt = 0  # 등장 빈도수가 threshold보다 작은 단어의 개수를 카운트
+    total_freq = 0  # 훈련 데이터의 전체 단어 빈도수 총 합
+    rare_freq = 0  # 등장 빈도수가 threshold보다 작은 단어의 등장 빈도수의 총 합
+
+    # 단어와 빈도수의 쌍(pair)을 key와 value로 받는다.
+    for key, value in tokenizer.word_counts.items():
+        total_freq = total_freq + value
+
+        # 단어의 등장 빈도수가 threshold보다 작으면
+        if (value < threshold):
+            rare_cnt = rare_cnt + 1
+            rare_freq = rare_freq + value
+
+    vocab_size = total_cnt - rare_cnt + 1
+
+    tokenizer = Tokenizer(vocab_size)
+    tokenizer.fit_on_texts(X_train)
+    # 2차
+    X_train = tokenizer.texts_to_sequences(X_train)
+    X_test = tokenizer.texts_to_sequences(X_test)
+
+    return X_train, X_test, tokenizer
+
+
+def post_processing():
+    global X_train, y_train, X_test, y_test
+    drop_train = [index for index, sentence in enumerate(X_train) if len(sentence) < 1]
+    # 빈 샘플들을 제거
+    X_train = np.delete(X_train, drop_train, axis=0)
+    y_train = np.delete(y_train, drop_train, axis=0)
+
+    drop_test = [index for index, sentence in enumerate(X_test) if len(sentence) < 1]
+    # 빈 샘플들을 제거
+    X_test = np.delete(X_test, drop_test, axis=0)
+    y_test = np.delete(y_test, drop_test, axis=0)
+
+    X_train = pad_sequences(X_train, maxlen=max_len)
+    X_test = pad_sequences(X_test, maxlen=max_len)
+
+
+def make_model():
+    model = Sequential()
+    model.add(Embedding(vocab_size, 100))
+    model.add(LSTM(128))
+    model.add(Dense(1, activation='sigmoid'))
+
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=3)
+    mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+    history = model.fit(X_train, y_train, epochs=15, callbacks=[es, mc], batch_size=60, validation_split=0.2)
+
+    return load_model('best_model.h5')
+
+
+def sentiment_predict(new_sentence):
+    new_sentence = okt.morphs(new_sentence, stem=True)  # 토큰화
+    new_sentence = [word for word in new_sentence if not word in stopwords]  # 불용어 제거
+    encoded = tokenizer.texts_to_sequences([new_sentence])  # 정수 인코딩
+    pad_new = pad_sequences(encoded, maxlen=max_len)  # 패딩
+    score = float(loaded_model.predict(pad_new))  # 예측
+    if (score > 0.5):
+        print("{:.2f}% 확률로 긍정 리뷰입니다.\n".format(score * 100))
+    else:
+        print("{:.2f}% 확률로 부정 리뷰입니다.\n".format((1 - score) * 100))
+
+
+if __name__ == "__main__":
+    train_data, test_data = load_data()
+
+    cleaning_data()
+
+    X_train, X_test = tokenize_data()
+
+    vocab_size = 0
+    X_train, X_test, tokenizer = encoding_data()
+
+    y_train = np.array(train_data['label'])
+    y_test = np.array(test_data['label'])
+    max_len = 30
+    post_processing()
+
+    loaded_model = make_model()
+
+    sentiment_predict('이 영화 핵노잼 ㅠㅠ')
---
+++ src/predict_file.py
@@ -0,0 +1,90 @@
+# -*- coding:utf-8 -*-
+import pandas as pd
+from konlpy.tag import Okt
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+import pickle
+
+okt = Okt()
+max_len = 30
+
+
+def load_stopwords():
+    fp = open("../resource/stopwords_kor.txt", "r", encoding='UTF8')
+    lines = fp.readlines()
+    fp.close()
+    df = pd.DataFrame(columns=["words", "pos", "prob"])
+    i = 0
+    for line in lines:
+        # print(line.strip().split('\t'))
+        df.loc[i] = line.strip().split('\t')
+        i += 1
+
+    # print(type(df['words'].tolist()))
+
+    return df, df['words'].tolist()
+
+
+def load_token():
+    fp = open("../resource/news/new_token.dat", "rb")
+    data = pickle.load(fp)
+    fp.close()
+
+    return data
+
+
+# Read File
+def read_file():
+    fp = open("../data/data_test.txt", "r", encoding='UTF8')
+    lines = fp.readlines()
+    fp.close()
+
+    return lines
+
+
+# Filtering
+def sentiment_predict(new_sentence):
+    new_sentence = okt.morphs(new_sentence, stem=True)  # 토큰화
+    new_sentence = [word for word in new_sentence if not word in stopwords]  # 불용어 제거
+    encoded = tokenizer.texts_to_sequences([new_sentence])  # 정수 인코딩
+    pad_new = pad_sequences(encoded, maxlen=max_len)  # 패딩
+    score = float(loaded_model.predict(pad_new))  # 예측
+    if score > 0.5:
+        print("{:.2f}% 확률로 긍정 리뷰입니다.\n".format(score * 100))
+        return 1, 0
+    else:
+        print("{:.2f}% 확률로 부정 리뷰입니다.\n".format((1 - score) * 100))
+        return 0, 1
+
+
+def predict_file(lines):
+    gv, nv = 0, 0
+    for line in lines:
+        line = line.replace("[^ㄱ-ㅎㅏ-ㅣ가-힝 ]","")
+        if len(line.strip()) < 3: continue
+        print(line)
+        gvt, nvt = sentiment_predict(line)
+        gv += gvt
+        nv += nvt
+
+    return gv, nv
+
+
+def main():
+    print("1. 일단 파일 읽자")
+    lines = read_file()
+    print("2. Filtering/Transform/Cleaning")
+    gv, nv = predict_file(lines)
+    if gv > nv:
+        print("긍정 :", gv, nv)
+    elif gv == nv:
+        print("중립 :", gv, nv)
+    else:
+        print("부정 :", gv, nv)
+
+
+if __name__ == "__main__":
+    df_sw, stopwords = load_stopwords()
+    tokenizer = load_token()
+    loaded_model = load_model("../resource/news/best_model.h5")
+    main()
